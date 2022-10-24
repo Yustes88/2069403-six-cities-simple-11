5be70377ff1ad34efb5e93ff10a3bc2e911939e4
Yulya <iuliiaobr.io@gmail.com>
2022-10-24 16:40:42 +0700

    Добавляет маршруты в app

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,8 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import NotFound from '../../pages/404/not-found';
+import Login from '../../pages/login/login';
 import MainPage from '../../pages/main-page/main-page';
+import Property from '../../pages/property/property';
 
 type AppScreenProps = {
   totalAmount: number;
@@ -6,7 +10,16 @@ type AppScreenProps = {
 
 function App({totalAmount}: AppScreenProps): JSX.Element {
   return (
-    <MainPage totalAmount={totalAmount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<MainPage totalAmount={totalAmount}/>}/>
+          <Route path='login' element={<Login/>}/>
+          <Route path='offer/:id' element={<Property/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
