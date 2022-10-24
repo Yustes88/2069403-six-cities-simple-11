bde9e8fcb504c3eb04a140563612764d3dbf7b5f
Yulya <iuliiaobr.io@gmail.com>
2022-10-24 18:39:21 +0700

    Переименовывает компоненты согласно ТЗ

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,8 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import NotFound from '../../pages/404/not-found';
 import Login from '../../pages/login/login';
-import MainPage from '../../pages/main-page/main-page';
-import Property from '../../pages/property/property';
+import Main from '../../pages/main/main';
+import Room from '../../pages/room/room';
 
 type AppScreenProps = {
   totalAmount: number;
@@ -13,9 +13,9 @@ function App({totalAmount}: AppScreenProps): JSX.Element {
     <BrowserRouter>
       <Routes>
         <Route path='/'>
-          <Route index element={<MainPage totalAmount={totalAmount}/>}/>
+          <Route index element={<Main totalAmount={totalAmount}/>}/>
           <Route path='login' element={<Login/>}/>
-          <Route path='offer/:id' element={<Property/>}/>
+          <Route path='offer/:id' element={<Room/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Route>
       </Routes>
