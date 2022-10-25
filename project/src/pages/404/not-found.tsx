67029d1b835f09b4a85aa7cc8c8577ee710fdb9b
Yulya <iuliiaobr.io@gmail.com>
2022-10-25 09:52:11 +0700

    Добавляет лого в компонент 404 и заменяет a href на Link to

--- project/src/pages/404/not-found.tsx
+++ project/src/pages/404/not-found.tsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function NotFound(): JSX.Element {
   return (
-    <section>
-      <h1>404. Page not found</h1>
-      <Link to="/">Вернуться на главную</Link>
-    </section>
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            {<Logo/>}
+          </div>
+        </div>
+      </header>
+      <main className="container page__main page__main--index">
+        <h1>404. Page not found</h1>
+        <Link to="/">Вернуться на главную</Link>
+      </main>
+    </>
   );
 }
 
--- project/src/pages/room/room.tsx
+++ project/src/pages/room/room.tsx
@@ -6,9 +6,7 @@ function Room():JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              {<Logo/>}
-            </div>
+            {<Logo/>}
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
