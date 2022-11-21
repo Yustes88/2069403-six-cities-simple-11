006f83a9998e21b9196e9e89db901890913896d9
Yulya <iuliiaobr.io@gmail.com>
2022-11-21 20:13:18 +0700

    Вкладывает селекторы

--- project/src/store/selectors.ts
+++ project/src/store/selectors.ts
@@ -1,27 +1,31 @@
 import { filteredOffersByCity } from '../utils/utils';
 import { City, Offers } from '../types/types';
+import { createSelector } from '@reduxjs/toolkit';
 
 export const getFilteredOffers = (state: {
   currentCity: City;
   offersList: Offers;
-}) => filteredOffersByCity(state.currentCity, state.offersList);
-
-
-export const getSortedOffers = (state: {
-  currentSorting: string;
-  offersList: Offers;
-  currentCity: City;
-},
-) => {
-  const filteredOffers = getFilteredOffers(state);
-  switch (state.currentSorting) {
-    case 'Price: low to high':
-      return filteredOffers.sort((a, b) => a.price - b.price);
-    case 'Price: high to low':
-      return filteredOffers.sort((a, b) => b.price - a.price);
-    case 'Top rated first':
-      return filteredOffers.sort((a, b) => b.rating - a.rating);
-    default:
-      return filteredOffers;
-  }
+}) => {
+  const filteredOffers = filteredOffersByCity(state.currentCity, state.offersList);
+  return filteredOffers;
 };
+
+
+const getSortingName = (state: { currentSorting: string}) => state.currentSorting;
+
+export const getSortedOffers = createSelector(
+  getFilteredOffers,
+  getSortingName,
+  (filteredOffers, currentSorting) => {
+    switch (currentSorting) {
+      case 'Price: low to high':
+        return filteredOffers.sort((a, b) => a.price - b.price);
+      case 'Price: high to low':
+        return filteredOffers.sort((a, b) => b.price - a.price);
+      case 'Top rated first':
+        return filteredOffers.sort((a, b) => b.rating - a.rating);
+      default:
+        return filteredOffers;
+    }
+  }
+);
