d2ae5d473cb7b446da945e53c51c68bdb5c6c519
Yulya <iuliiaobr.io@gmail.com>
2022-11-26 18:46:05 +0700

    Реализует смену header при клике на sign out

--- project/src/components/header-logout/header-logout.tsx
+++ project/src/components/header-logout/header-logout.tsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom';
+import { AuthorizationStatus } from '../../const';
+import { store } from '../../store';
+import { requireAuthorization } from '../../store/action';
 import HeaderProfile from '../header-user/header-user';
 
 
 function HeaderLogout():JSX.Element {
+
+  const handleOnLogoutClick = () => {
+    store.dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+  };
+
   return (
     <>
       <HeaderProfile/>
       <li className="header__nav-item">
-        <Link className="header__nav-link" to="#link">
+        <Link className="header__nav-link" to="#link" onClick={handleOnLogoutClick}>
           <span className="header__login">Sign out</span>
         </Link>
       </li>
