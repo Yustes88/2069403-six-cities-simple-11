14f401a9473af2f25a45c1a4787064202301c974
Yulya <iuliiaobr.io@gmail.com>
2022-12-14 19:24:19 +0700

    Отображает фото на странице логин

--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -71,39 +71,40 @@ function Login():JSX.Element {
       <Helmet>
         <title>Login</title>
       </Helmet>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <Logo/>
-          </div>
-        </div>
-      </header>
-
-      <main className="page__main page__main--login">
-        <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post" onSubmit={handleFormSubmit}>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input ref={emailRef} className="login__input form__input" type="email" name="email" placeholder="Email" required/>
-              </div>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input ref={passwordRef} className="login__input form__input" type="password" name="password" placeholder="Password" required/>
-              </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
-            </form>
-          </section>
-          <section className="locations locations--login locations--current">
-            <div className="locations__item">
-              <Link className="locations__item-link" to={AppRoute.Root} onClick={handleLocationClick}>
-                <span>{randomCity.name}</span>
-              </Link>
+      <div className="page page--gray page--login">
+        <header className="header">
+          <div className="container">
+            <div className="header__wrapper">
+              <Logo/>
             </div>
-          </section>
-        </div>
-      </main>
+          </div>
+        </header>
+        <main className="page__main page__main--login">
+          <div className="page__login-container container">
+            <section className="login">
+              <h1 className="login__title">Sign in</h1>
+              <form className="login__form form" action="#" method="post" onSubmit={handleFormSubmit}>
+                <div className="login__input-wrapper form__input-wrapper">
+                  <label className="visually-hidden">E-mail</label>
+                  <input ref={emailRef} className="login__input form__input" type="email" name="email" placeholder="Email" required/>
+                </div>
+                <div className="login__input-wrapper form__input-wrapper">
+                  <label className="visually-hidden">Password</label>
+                  <input ref={passwordRef} className="login__input form__input" type="password" name="password" placeholder="Password" required/>
+                </div>
+                <button className="login__submit form__submit button" type="submit">Sign in</button>
+              </form>
+            </section>
+            <section className="locations locations--login locations--current">
+              <div className="locations__item">
+                <Link className="locations__item-link" to={AppRoute.Root} onClick={handleLocationClick}>
+                  <span>{randomCity.name}</span>
+                </Link>
+              </div>
+            </section>
+          </div>
+        </main>
+      </div>
     </>
   );
 }
